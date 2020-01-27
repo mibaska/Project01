abc229aee79a1d1d7ec32ff2bb80e61899f59b49
Spencer Wood <spencer.wood@me.com>
2020-01-26 21:36:27 -0600

    fixed all the Ajax issues with calls

--- rick-script.js
+++ rick-script.js
@@ -12,53 +12,54 @@ $.ajax({
 
      //varibles for Answers
     console.log(response.results[10].origin.name, 'chacater question 1'); // Character Question 1
-    const charAnswer1 = response.reuslt[10].origin.name;
+    const charAnswer1 = response.results[10].origin.name;
 
-    console.log(response.results[0].origin.name , 'Location question 1'); // Location Question 1
     const locAnswer1 = response.results[0].origin.name;
+    console.log(response.results[0].origin.name , 'Location question 1'); // Location Question 1
 
     console.log(response.results[13].location.name , ' location question 2'); //Location Question 2
     const locAnswer2 = response.results[13].location.name;
-    console.log(respnose.results[2].origin.name , 'location question 3');//location question 3
+
+    console.log(response.results[2].origin.name , 'location question 3');//location question 3
     const locAnswer3 = response.results[2].origin.name;
 
 ;
 
-//Call for Rick Answer
-$.ajax({
-  url: 'https://rickandmortyapi.com/api/character?name=rick',
-  method: 'GET'
-})
-.then (function(rickResponse){
-  console.log(rickresponse.info.count, ' character question 2'); //Character Question 2 
-  //varibles for Answer
-  charAnswer2 = rickResponse.info.count ;
-});
+    //Call for Rick Answer
+    $.ajax({
+      url: 'https://rickandmortyapi.com/api/character?name=rick',
+      method: 'GET'
+    })
+    .then (function(rickResponse){
+      console.log(rickResponse.info.count, ' character question 2'); //Character Question 2 
+      //varibles for Answer
+      charAnswer2 = rickResponse.info.count ;
+    ;
 
-//Call for Morty Answer
-$.ajax({
-  url: 'https://rickandmortyapi.com/api/character?name=morty',
-  method: 'GET'
-})
-.then (function(mortyResponse){
-  console.log(mortyResponse.info.count, 'chacter question 3'); //Character Question 3
-  //varibles for Answer
-  charAnswer3 = mortyResponse.info.count ;
-});
+      //Call for Morty Answer
+      $.ajax({
+        url: 'https://rickandmortyapi.com/api/character?name=morty',
+        method: 'GET'
+      })
+      .then (function(mortyResponse){
+        console.log(mortyResponse.info.count, 'chacter question 3'); //Character Question 3
+        //varibles for Answer
+        charAnswer3 = mortyResponse.info.count ;
+;
 
-//Call for episode questions
-$.ajax({
-  url: 'https://rickandmortyapi.com/api/episode?=S01',
-  method: 'GET'
-})
-.then (function(epResponse){
-  console.log(ep1Response.id[1].air_date, 'Date Question 1'); //Date Question 1
-  console.log(reponse.info.count , 'date question 2'); // Date Question 2
-  //varibles for Answer
-  epQuestion1 = epResponse.id[1].air_date;
-  epQuestion2 = epResponse.info.count;
+        //Call for episode questions
+        $.ajax({
+          url: 'https://rickandmortyapi.com/api/episode?=S01',
+          method: 'GET'
+        })
+        .then (function(epResponse){
+          console.log(epResponse.results[0].air_date, 'Date Question 1'); //Date Question 1
+          console.log(epResponse.info.count , 'Date question 2'); // Date Question 2
+          //varibles for Answer
+          epQuestion1 = epResponse.results[0].air_date;
+          epQuestion2 = epResponse.info.count;
 
-});
+        ;
 
 //counter for items
 //keep score
@@ -70,15 +71,9 @@ $.ajax({
 //make answer all lowercase
 //compare user answer to correct answer
 
-//functions for displaying questions
-$('#parentChar2').click(function(){
-  $('#parentChar2').unbind();
-  $("#char2").text(chrArray[0].title);
-});
-
 
 //array for character questions
-const chrArray = [
+var chrArray = [
     {
       title: "What planet was Albert Einstein from? ",
       //pull answer from api
@@ -98,11 +93,12 @@ const chrArray = [
 
 ];
 //array for locations questions
-const locArray = [
+var locArray = [
     {
       title: "What planet is Rick Sanchez from?",
       //pull answer from api
-      answer: locAnswer1 //Earth (C-137)
+      answer: locAnswer1  //Earth (C-137)
+      
     }, 
     {
       title: "What is the location of Alien Morty?",
@@ -116,7 +112,7 @@ const locArray = [
     },
 ];
 // Array to get random questions
-const randomArray = [
+var randomArray = [
     {
       title: "What year did the pilot release?",
      //pull answer from api
@@ -132,13 +128,25 @@ const randomArray = [
       //pull answer from api
       answer: "female" //count male = 371 female = 74
     },
-
 ];
-
-
-});
+  });
+    });
+      });
+        });
 //pass questions to HTML
 
+//functions for displaying questions
+$(function () {
+  $('#parentChar2').click(function(){
+    $('#parentChar2').unbind();
+     $("#char2").text(chrArray[0].title);
+  });
+  $('#parentPlace2').click(function(){
+      $('#parentPlace2').unbind();
+      $('#place2').text(locArray[0].title);
+  });
+});
+  
 //submit answer to check
 $('button').on('click', function(event){
     //variable for user answer
